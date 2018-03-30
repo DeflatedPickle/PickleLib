aa24be81e6e6aed43f1e23815503758cee00cef0
DeflatedPickle <DeflatedPickle@gmail.com>
2018-03-30 18:30:08 +0100

    Fixed main class

--- src/main/java/com/deflatedpickle/picklelib/PickleLib.java
+++ src/main/java/com/deflatedpickle/picklelib/PickleLib.java
@@ -3,7 +3,7 @@ package com.deflatedpickle.picklelib;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.Instance;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS, dependencies = "required-after:autoreglib;")
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS)
 public class PickleLib {
     @Instance
     public static PickleLib instance;
