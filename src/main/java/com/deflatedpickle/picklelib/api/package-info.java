6dee1193ae51bdc22e4aef532badc546750500c4
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-05 22:53:32 +0100

    Added a package info file

---
+++ src/main/java/com/deflatedpickle/picklelib/api/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = Reference.NAME, apiVersion = Reference.VERSION, provides = "PickleLib|API")
+package com.deflatedpickle.picklelib.api;
+
+import com.deflatedpickle.picklelib.Reference;
+import net.minecraftforge.fml.common.API;
