e17f89533244f85f22fdf847dcc605e67803d893
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-09 14:32:35 +0100

    Added a BlockBase class

---
+++ src/main/java/com/deflatedpickle/picklelib/block/BlockBase.java
@@ -0,0 +1,25 @@
+package com.deflatedpickle.picklelib.block;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import org.apache.commons.lang3.tuple.Pair;
+import vazkii.arl.block.BlockMod;
+
+public class BlockBase extends BlockMod {
+    private String _nameSpace;
+
+    public BlockBase(String name, Material materialIn, Float hardness, Float resistance, Pair<String, Integer> harvestLevel, CreativeTabs tab, String nameSpace, String... variants) {
+        super(name, materialIn, variants);
+        _nameSpace = nameSpace;
+
+        setHardness(hardness);
+        setResistance(resistance);
+        setHarvestLevel(harvestLevel.getLeft(), harvestLevel.getRight());
+        setCreativeTab(tab);
+    }
+
+    @Override
+    public String getModNamespace() {
+        return _nameSpace;
+    }
+}
