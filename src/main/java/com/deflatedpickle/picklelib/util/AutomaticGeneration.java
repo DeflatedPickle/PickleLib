6535f2d8e1286131bf961f1e902c1b49e908d0ef
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-09 15:22:02 +0100

    Added a genWoodResources method

--- src/main/java/com/deflatedpickle/picklelib/util/AutomaticGeneration.java
+++ src/main/java/com/deflatedpickle/picklelib/util/AutomaticGeneration.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class AutomaticGeneration {
     public List<Object> genMineralResources(String name, CreativeTabs creativeTab, String nameSpace) {
         // Generates; Ore, Ingot and a Nugget for the mineral.
-        BlockBase ore = new BlockBase(name + "_ore", Material.ROCK, 3.0f, 15.0f, new ImmutablePair<>("pickaxe", 1), creativeTab, nameSpace);
+        BlockBase ore = new BlockBase(name + "_ore", Material.ROCK, 3.0f, 15.0f, ImmutablePair.of("pickaxe", 1), creativeTab, nameSpace);
         ItemBase ingot = new ItemBase(name + "_ingot", 64, creativeTab, nameSpace);
         ItemBase nugget = new ItemBase(name + "_nugget", 64, creativeTab, nameSpace);
 
@@ -26,5 +26,13 @@ public class AutomaticGeneration {
         ItemBase meatCooked = new ItemBase(name + "_meatCooked", 64, creativeTab, nameSpace);
 
         return Arrays.asList(hide, meatRaw, meatCooked);
+    }
+
+    public List<Object> genWoodResources(String name, CreativeTabs creativeTab, String nameSpace) {
+        BlockBase log = new BlockBase(name + "_log", Material.WOOD, 2f, 10f, ImmutablePair.of("axe", 0), creativeTab, nameSpace);
+        BlockBase leaves = new BlockBase(name + "_leaves", Material.WOOD, 0.2f, 1f, ImmutablePair.of("shears", 0), creativeTab, nameSpace);
+        BlockBase planks = new BlockBase(name + "_planks", Material.WOOD, 2f, 15f, ImmutablePair.of("axe", 0), creativeTab, nameSpace);
+
+        return Arrays.asList(log, leaves, planks);
     }
 }
