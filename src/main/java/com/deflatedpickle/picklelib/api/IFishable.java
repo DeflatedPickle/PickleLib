65937b11766fdfbe1e9e05756a6b71e22b63f223
DeflatedPickle <DeflatedPickle@gmail.com>
2018-10-15 20:25:52 +0100

    Removed unnecessary files

--- src/main/java/com/deflatedpickle/picklelib/api/IFishable.java
+++
@@ -1,7 +0,0 @@
-package com.deflatedpickle.picklelib.api;
-
-public interface IFishable {
-    default boolean isFishable() {
-        return true;
-    }
-}
--- src/main/java/com/deflatedpickle/picklelib/tabs/TabBase.java
+++ src/main/java/com/deflatedpickle/picklelib/tabs/TabBase.java
@@ -1,17 +1,14 @@
 package com.deflatedpickle.picklelib.tabs;
 
-import com.deflatedpickle.picklelib.PickleLib;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 
 public class TabBase extends CreativeTabs {
-    private String label;
     private ItemStack icon;
     private Boolean searchBar;
 
-    public TabBase(String label, ItemStack icon, Boolean searchBar) {
-        super("tab_"+ PickleLib.getNameSpace() + "_" + label);
-        this.label = label;
+    public TabBase(String nameSpace, String label, ItemStack icon, Boolean searchBar) {
+        super("tab_"+ nameSpace + "_" + label);
         this.icon = icon;
         this.searchBar = searchBar;
     }
