d79c6825e3a924f24e3124cc34563b6977c5a062
DeflatedPickle <DeflatedPickle@gmail.com>
2018-10-15 20:30:55 +0100

    Updated version

--- src/main/java/com/deflatedpickle/picklelib/Reference.java
+++ src/main/java/com/deflatedpickle/picklelib/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
     public static final String MOD_ID = "picklelib";
     public static final String NAME = "PickleLib";
     // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
-    public static final String VERSION = "1.12.2-1.0.15.2";
+    public static final String VERSION = "1.12.2-1.0.16.0";
     public static final String ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]";
 }
